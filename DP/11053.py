027ccb84b89ca634155c263d11f3664388f323eb
seohyun0120 <sarahseohyun@gmail.com>
2020-10-06 00:17:18 +0900

    가장 긴 증가하는 부분 수열

--- DP/11053.py
+++ DP/11053.py
@@ -2,12 +2,11 @@ x = int(input())
 
 case = list(map(int, input().split()))
 
-dp = [0 for i in range(x+1)]
+dp = [1 for i in range(x)]
 
 for i in range(x):
-    dp[i] = 1
     for j in range(i):
-        if case[i] > case[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+        if case[i] < case[j]:
+            dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
