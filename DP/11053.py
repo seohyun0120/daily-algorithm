60d6bac9fbc87904d3eb0d5425520a673a132620
seohyun0120 <sarahseohyun@gmail.com>
2020-10-07 23:42:17 +0900

    소스 수정

--- DP/11053.py
+++ DP/11053.py
@@ -6,7 +6,7 @@ dp = [1 for i in range(x)]
 
 for i in range(x):
     for j in range(i):
-        if case[i] < case[j]:
+        if case[i] > case[j]:
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
