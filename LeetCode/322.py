6131923ad39e0a2e832ac586ceaa1f0e177955e0
seohyun0120 <sarahseohyun@gmail.com>
2020-12-29 22:15:33 +0900

    Coin Change

---
+++ LeetCode/322.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        dp = [0] + [float(inf)] * amount
+        
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
+
+        if dp[-1] != float(inf):
+            return dp[-1]
+        else:
+            return -1
